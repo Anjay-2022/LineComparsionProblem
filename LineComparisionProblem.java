97acd242f59a914bae13c272deab72849a3c61ba
Anjay-2022 <anjay2013@gmail.com>
2022-11-01 22:18:39 +0530

    first commit master

---
+++ LineComparisionProblem.java
@@ -0,0 +1,8 @@
+package assignment2;
+
+
+public class LineComparisionProblem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}    
+}
